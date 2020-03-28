d22da18e863e9a9da62b913716db66bc05a1f47e
yasuturing <yasuturing@gmail.com>
2020-03-28 17:01:47 +0900

    first commit

---
+++ lightspeed.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	const lightSpeed = 299792 // km/秒
+	var distance = 56000000 //km
+	fmt.Println(distance/lightSpeed, "秒")
+
+	
+	fmt.Println(distance/lightSpeed, "秒")
+}
