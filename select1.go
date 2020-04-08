d1278aef17cb1ea789895d9fe58c7515ad7393d1
yasuturing <yasuturing@gmail.com>
2020-04-08 14:28:38 +0900

    未完成

--- select1.go
+++ select1.go
@@ -10,7 +10,7 @@ func main55() {
 	for i := 0; i < 5; i++ {
 		select {
 		case gopherID := <-c: //gopherの起床待ち
-		fmt.Println("gopher " , gopherID, "はスリープを終えました”)
+			fmt.Println("gopher ", gopherID, "はスリープを終えました")
 		case <-timeout:
 			fmt.Println("忍耐の限界に達した！")
 			return
